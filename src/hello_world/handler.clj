c9366f812a4bfe32d38bcdf417483bed9ec37781
Davi Suga <daviciencia1@gmail.com>
2022-08-04 19:21:25 -0300

    feat: added java library interop

--- src/hello_world/handler.clj
+++ src/hello_world/handler.clj
@@ -1,10 +1,14 @@
 (ns hello-world.handler
+  (:import (org.opencypher.gremlin.translation TranslationFacade CypherAst))
   (:require [compojure.core :refer :all]
             [compojure.route :as route]
             [ring.middleware.defaults :refer [wrap-defaults site-defaults]]))
 
+(defn translate [mode cypher] (.toGremlinGroovy (new TranslationFacade)))
+
 (defroutes app-routes
   (GET "/" [] "Hello World")
+  (POST "/to-gremlin" [request] translate)
   (route/not-found "Not Found"))
 
 (def app
